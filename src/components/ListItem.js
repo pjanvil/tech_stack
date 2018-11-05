fb942b1764fe1a884ee0e0ac3a3f3cf606cab8e3
Paul Jones <puj@Pauls-MacBook-Air.local>
2018-11-05 13:11:46 +0000

    lesson 101

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,28 @@
 import React, {Component} from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import {
+    Text,
+    TouchableWithoutFeedback,
+    View,
+    LayoutAnimation
+} from 'react-native';
+
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
-    renderDescription() {
-        const { library, selectedLibraryId} = this.props;
+    componentWillUpdate() {
+        LayoutAnimation.spring();
+    }
 
-        if (library.item.id === selectedLibraryId) {
+    renderDescription() {
+        const { library, expanded} = this.props;
+
+        if (expanded) {
             return (
-                <Text>{this.props.library.item.description}</Text>
+                <CardSection>
+                    <Text style={{flex: 1}}>{library.item.description}</Textstyle>
+                </CardSection>
             );
         }
     }
@@ -45,8 +57,10 @@ const styles = {
     }
 }
 
-const mapStateToProps = state => {
-    return { selectedLibraryId: state.selectedLibraryId };
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.item.id;
+
+    return { expanded };
 }
 
 export default connect(mapStateToProps, actions)(ListItem);
